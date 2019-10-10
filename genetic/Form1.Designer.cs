ee5bf7745bf668f831b8f93bc05f8a34b6b5076c
vilya1997@bk.ru <vilya1997@bk.ru>
2019-10-10 14:15:24 +0300

    Start population

---
+++ genetic/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace genetic
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.nudCount = new System.Windows.Forms.NumericUpDown();
+      this.calc = new System.Windows.Forms.Button();
+      this.label3 = new System.Windows.Forms.Label();
+      this.dgv = new System.Windows.Forms.DataGridView();
+      this.clean = new System.Windows.Forms.Button();
+      this.generate = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.nudCount)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(13, 13);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(161, 20);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Количество особей:";
+      // 
+      // nudCount
+      // 
+      this.nudCount.Location = new System.Drawing.Point(12, 37);
+      this.nudCount.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+      this.nudCount.Name = "nudCount";
+      this.nudCount.Size = new System.Drawing.Size(162, 26);
+      this.nudCount.TabIndex = 2;
+      this.nudCount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+      // 
+      // calc
+      // 
+      this.calc.Location = new System.Drawing.Point(12, 112);
+      this.calc.Name = "calc";
+      this.calc.Size = new System.Drawing.Size(162, 37);
+      this.calc.TabIndex = 10;
+      this.calc.Text = "Рассчитать";
+      this.calc.UseVisualStyleBackColor = true;
+      this.calc.Click += new System.EventHandler(this.calc_Click);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(245, 13);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(104, 20);
+      this.label3.TabIndex = 11;
+      this.label3.Text = "Результаты:";
+      // 
+      // dgv
+      // 
+      this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dgv.Location = new System.Drawing.Point(249, 37);
+      this.dgv.Name = "dgv";
+      this.dgv.RowTemplate.Height = 28;
+      this.dgv.Size = new System.Drawing.Size(757, 401);
+      this.dgv.TabIndex = 12;
+      // 
+      // clean
+      // 
+      this.clean.Location = new System.Drawing.Point(12, 155);
+      this.clean.Name = "clean";
+      this.clean.Size = new System.Drawing.Size(162, 37);
+      this.clean.TabIndex = 13;
+      this.clean.Text = "Очистить";
+      this.clean.UseVisualStyleBackColor = true;
+      this.clean.Click += new System.EventHandler(this.clean_Click);
+      // 
+      // generate
+      // 
+      this.generate.Location = new System.Drawing.Point(12, 69);
+      this.generate.Name = "generate";
+      this.generate.Size = new System.Drawing.Size(162, 37);
+      this.generate.TabIndex = 14;
+      this.generate.Text = "Сгенерировать";
+      this.generate.UseVisualStyleBackColor = true;
+      this.generate.Click += new System.EventHandler(this.generate_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1018, 450);
+      this.Controls.Add(this.generate);
+      this.Controls.Add(this.clean);
+      this.Controls.Add(this.dgv);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.calc);
+      this.Controls.Add(this.nudCount);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Генетический алгоритм";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.nudCount)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.NumericUpDown nudCount;
+    private System.Windows.Forms.Button calc;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.DataGridView dgv;
+    private System.Windows.Forms.Button clean;
+    private System.Windows.Forms.Button generate;
+  }
+}
+
---
+++ genetic/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace genetic
+{
+  public partial class Form1 : Form
+  {
+    public int Population;
+
+    private int function(Point point)
+    {
+      int F;
+      F = -point.X * point.X - 5 * point.Y * point.Y + 10 * point.X + (20 * point.Y) + 400;
+      return F;
+    }
+
+    private int functionX(int x)
+    {
+      int F;
+      F = -2 * x + 10;
+      return F;
+    }
+
+    private int functionY(int y)
+    {
+      int F;
+      F = -10 * y + 20;
+      return F;
+    }
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void calc_Click(object sender, EventArgs e)
+    {
+
+
+    }
+
+    private string decToBin(int dec)
+    {
+      return Convert.ToString(dec, 2).PadLeft(5, '0');
+    }
+
+    private int binToDec(string bin)
+    {
+      return Convert.ToInt32("0b" + bin);
+    }
+
+    private string stick(Point point)
+    {
+      return decToBin(point.X) + decToBin(point.Y);
+    }
+
+    private Point unstick(string chromosome)
+    {
+      Point point = new Point(binToDec(chromosome.Substring(0, 5)), binToDec(chromosome.Substring(5)));
+      return point;
+    }
+
+    private void clean_Click(object sender, EventArgs e)
+    {
+      dgv.ColumnCount = 0;
+      dgv.RowCount = 1;
+      nudCount.Enabled = true;
+    }
+
+    private void generate_Click(object sender, EventArgs e)
+    {
+      Population = Convert.ToInt32(nudCount.Value);
+      nudCount.Enabled = false;
+      dgv.RowCount = Population + 4;
+      dgv.ColumnCount = 13;
+      dgv.Columns[0].Name = "№";
+      dgv.Columns[1].Name = "decimal X";
+      dgv.Columns[2].Name = "decimal Y";
+      dgv.Columns[3].Name = "binary X";
+      dgv.Columns[4].Name = "binary Y";
+      dgv.Columns[5].Name = "chromosome";
+      dgv.Columns[6].Name = "F(X,Y)";
+      dgv.Columns[7].Name = "F(X,Y)/Sum";
+      dgv.Columns[8].Name = "Copies planned";
+      dgv.Columns[9].Name = "Copies real";
+      dgv.Columns[10].Name = "Pairs";
+      dgv.Columns[11].Name = "Breakpoint";
+      dgv.Columns[12].Name = "Children";
+      dgv[0, dgv.RowCount - 3].Value = "Sum";
+      dgv[0, dgv.RowCount - 2].Value = "Average";
+      dgv[0, dgv.RowCount - 1].Value = "Max";
+      Random random = new Random();
+      for (int i = 0; i < Population; i++)
+      {
+        dgv[0, i].Value = i + 1;
+        dgv[1, i].Value = random.Next(32);
+        dgv[2, i].Value = random.Next(32);
+        dgv[3, i].Value = decToBin(Convert.ToInt32(dgv[1, i].Value));
+        dgv[4, i].Value = decToBin(Convert.ToInt32(dgv[2, i].Value));
+        Point point = new Point(Convert.ToInt32(dgv[1, i].Value), Convert.ToInt32(dgv[2, i].Value));
+        dgv[5, i].Value = stick(point);
+        dgv[6, i].Value = function(point);
+      }
+      int sum = 0;
+      int max = 0;
+      for (int i = 0; i < Population; i++)
+      {
+        sum += Convert.ToInt32(dgv[6, i].Value);
+        if (Convert.ToInt32(dgv[6, i].Value) > max)
+        {
+          max = Convert.ToInt32(dgv[6, i].Value);
+        }
+      }
+      dgv[6, dgv.RowCount - 3].Value = sum;
+      dgv[6, dgv.RowCount - 2].Value = sum / Population;
+      dgv[6, dgv.RowCount - 1].Value = max;
+      for (int i = 0; i < Population + 1; i++)
+      {
+        dgv[7, i].Value = Convert.ToDouble(dgv[6, i].Value) / sum;
+        dgv[8, i].Value = Convert.ToDouble(dgv[7, i].Value) * Population;
+      }
+      for (int i = 0; i < Population; i++)
+      {
+        if (Convert.ToDouble(dgv[8, i].Value) > 0)
+        {
+          dgv[9, i].Value = Math.Round(Convert.ToDouble(dgv[8, i].Value));
+        }
+        else
+        {
+          dgv[9, i].Value = 0;
+        }
+        dgv[10, i].Value = Convert.ToDouble(dgv[7, i].Value) * Population;
+      }
+    }
+  }
+}
